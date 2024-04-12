7affc1a1e779122f933f094ec3229fdc7babce43
justin-corbett <hello@justincorbett.co.nz>
2024-04-12 15:56:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,6 @@ ScrollTrigger.defaults({
   markers: false,
 });
 
-// BG Video hide until loaded
-document.addEventListener('DOMContentLoaded', function() {
-    var videos = document.getElementsByClassName('video-bg');
-    for (var i = 0; i < videos.length; i++) {
-        videos[i].style.display = 'none'; // Hide the video initially
-        videos[i].addEventListener('loadeddata', function() {
-            this.style.display = 'block'; // Show the video when it's fully loaded
-            this.autoplay = true; // Autoplay the video
-            this.loop = true; // Set the video to loop
-            this.muted = true; // Mute the video
-            this.playsInline = true; // Ensure the video plays inline
-        });
-    }
-});
-
 // GSAP Horizontal scroll
 if ($(window).width() > 768) {
 let tlMain = gsap
