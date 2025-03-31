8eb938046c329ed8dcbd5974e38926c153682128
JC <120233340+justjuicenom@users.noreply.github.com>
2025-04-01 09:36:45 +1300

    Update index.js

--- index.js
+++ index.js
@@ -654,7 +654,7 @@ $(".scroll-track.is-home_hero").each(function (index) {
 
 
 // GSAP Navigation BG Gradient Fade In – Services
-$(".animate-nav-bg").each(function (index) {
+$(".section-hero-content").each(function (index) {
     let triggerElement = $(this);
     let targetElement = $(".navigation-bg-gradient");
   
@@ -662,8 +662,8 @@ $(".animate-nav-bg").each(function (index) {
       scrollTrigger: {
         trigger: triggerElement,
         // trigger element - viewport
-        start: "top bottom",
-        end: "top top",
+        start: "top top",
+        end: "top bottom",
         scrub: 1,
       },
     });
